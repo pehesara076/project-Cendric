7b691f09b17601d4a32be80b67ebce741c6bf581
Sivaraja Pratheep <sivarajapratheep175@gmail.com>
2026-09-25 06:49:56 +0530

    Delete backend directory

--- backend/routes/exportRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const transactionController = require('../controllers/transactionController');
-const { authenticateToken } = require('../middleware/auth');
-
-router.get('/csv', authenticateToken, transactionController.exportTransactionsCSV);
-
-module.exports = router;
